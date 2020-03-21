2423794c264cdf89a55225b0b2a4fbc2c295d91a
iii <zeltkimo+github@gmail.com>
2020-03-21 14:13:54 +0800

    Mar21 1413

--- app/src/main/java/tw/org/iii/brad/brad13/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad/brad13/MainActivity.java
@@ -1,41 +1,103 @@
 package tw.org.iii.brad.brad13;
-
+//連線狀態,BroadcastReceiver, IntentFilter
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 
 public class MainActivity extends AppCompatActivity {
     private ConnectivityManager cmgr;
     private MyReceiver myReceiver;
     private TextView mesg;
+    private ImageView img;
+    private boolean isAllowSDCard;
+    private File downloadDir;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED){      //若沒權限
+            ActivityCompat.requestPermissions(this,     //要權限
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+
+        }else{
+            isAllowSDCard = true;
+            init();
+        }
+
+    }
+
+    private void init(){
+        if(isAllowSDCard){
+            downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        }
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setMessage("Downloading...");    //設定好了但還沒有叫它show
+
         mesg = findViewById(R.id.mesg);
+        img = findViewById(R.id.img);
 
         cmgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);    //service是本來就有的所以get
         myReceiver = new MyReceiver();
-        IntentFilter filter = new IntentFilter(/*ConnectivityManager.CONNECTIVITY_ACTION*/);   //肚子裡的東西為Action,若只有一個action可放建構的參數裡面就好
+        IntentFilter filter = new IntentFilter(/*ConnectivityManager.CONNECTIVITY_ACTION*/);  //廣播接收器什麼都會收到,故需要filter過濾要的訊息 //肚子裡的東西為Action,若只有一個action可放建構的參數裡面就好
         filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);  //此方式可一直add,加上多個action
         filter.addAction("brad");   //要過濾哪個action,上方是定義好的網路連線,此行則是下方我們自己定義的action名為brad
-        registerReceiver(myReceiver, filter);
+        registerReceiver(myReceiver, filter);   //將接收器和過濾器註冊(掛載)
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            isAllowSDCard = true;
+        }else{
+            isAllowSDCard = false;
+        }
+        init();
     }
 
     @Override
@@ -108,5 +170,120 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }.start();
+    }
+
+    public void test4(View view){
+        new Thread(){
+            @Override
+            public void run() {
+                fetchImage();
+            }
+        }.start();
+    }
+
+    public void test5(View view) {
+        if (!isAllowSDCard) return;
+        Toast.makeText(this,"oh no",Toast.LENGTH_SHORT);
+
+
+        progressDialog.show();  //UI不能放執行序裡
+
+        new Thread(){
+            @Override
+            public void run() {
+                fetchPDF();
+            }
+        }.start();
+    }
+
+    public void test6(View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT,"Sharing URL");
+        intent.putExtra(Intent.EXTRA_TEXT,"http://www.url.com");
+        startActivity(Intent.createChooser(intent,"Share URL"));
+    }
+
+    private void fetchPDF(){
+        try {
+            URL url = new URL("https://pdfmyurl.com/?url=https://www.gamer.com.tw");
+            HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+            conn.setHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            conn.connect();
+
+
+
+            File downloadFile = new File(downloadDir,"gamer.pdf");
+            FileOutputStream fout = new FileOutputStream(downloadFile);
+
+            byte[] buf = new byte[1024*4096]; //4MB
+            BufferedInputStream bin = new BufferedInputStream(conn.getInputStream());
+
+            int len = -1;
+            while( (len = bin.read(buf)) != -1){
+                fout.write(buf,0,len);
+            }
+
+            bin.close();
+            fout.flush();
+            fout.close();
+
+            uiHandler.sendEmptyMessage(2);
+
+            Log.v("brad","save OK");
+        }catch (Exception e){
+            Log.v("brad",e.toString());
+        }finally {
+            uiHandler.sendEmptyMessage(1);
+        }
+    }
+
+    private Bitmap bmp;
+
+    private void fetchImage(){
+        try {
+            URL url = new URL("https://3.bp.blogspot.com/-NQ7KvxrdO1w/XZ2auy102sI/AAAAAAAAJ3I/B_BeovwcuF8WlCbwQZ8Wa1hNvbqqkF2jQCK4BGAYYCw/s1600/joker-2019-joaquin-phoenix-clown-5c.jpg");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.connect();
+
+//            1. conn.getInputStream();
+//            2. ImageView
+            bmp = BitmapFactory.decodeStream(conn.getInputStream()); //拿到Bitmap
+            uiHandler.sendEmptyMessage(0);
+
+
+        }catch (Exception e){
+
+        }
+
+    }
+
+    private UIHandler uiHandler = new UIHandler();
+
+    private class UIHandler extends Handler{
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 0) img.setImageBitmap(bmp);
+            if(msg.what == 1) progressDialog.dismiss(); //dismiss就看不到了 不用銷毀
+            if(msg.what == 2) showPDF();
+        }
+    }
+
+    private void showPDF(){
+        File file = new File(downloadDir,"gamer.pdf");
+        Uri pdfuri = FileProvider.getUriForFile(this,
+                getPackageName() + ".name",file);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(pdfuri,"application/pdf");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
     }
 }
